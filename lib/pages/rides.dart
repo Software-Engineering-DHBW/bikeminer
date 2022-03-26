a625b7052e00d3ff50b26b252600f91bda44afa7
b9-rzt <barztobi@gmail.com>
2022-03-26 15:07:54 +0100

    Flutter RidingHistory

--- lib/pages/rides.dart
+++ lib/pages/rides.dart
@@ -1,4 +1,6 @@
+import 'package:bikeminer/pages/ride.dart';
 import 'package:flutter/material.dart';
+import 'package:timelines/timelines.dart';
 
 class RidesPage extends StatefulWidget {
   const RidesPage({Key? key}) : super(key: key);
@@ -7,17 +9,107 @@ class RidesPage extends StatefulWidget {
   State<RidesPage> createState() => _RidesPageState();
 }
 
+/// Stateful Widget "RidesPageState" stellt das Grundgerüst der Seite zur gefahrenen Strecken anhand einer Timeline dar
 class _RidesPageState extends State<RidesPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "BikeMiner",
-          style: TextStyle(color: Colors.white),
+    const title = 'Rides';
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(backgroundColor: const Color.fromARGB(255, 30, 134, 49), title: const Text(title,style: TextStyle(color: Colors.white),),
+          /// Button in der AppBar um zur vorherigen Seite zu gelangen
+          automaticallyImplyLeading: false,
+          leading: IconButton (
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              /// Navigiere zur vorherigen Seite
+              Navigator.pop(context);
+            },
+          ),
         ),
-        backgroundColor: const Color.fromARGB(255, 30, 134, 49),
+        body: const RidingHistory(),
       ),
     );
   }
 }
+/// Der Inhalt (Body: "RidingHistory") der "RidesPageState"-Seite
+class RidingHistory extends StatelessWidget {
+  const RidingHistory({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ///Erstellung der Timeline (Zeitlinie teilt den Body der Seite in zwei Hälften) mit bestimmten Stil
+    return Timeline.tileBuilder(builder: TimelineTileBuilder.fromStyle(
+      contentsAlign: ContentsAlign.alternating,
+      contentsBuilder: (context, index) => Padding(
+        ///Skalierung/Abstand der Seitenränder
+        padding: const EdgeInsets.all(24.0),
+        /// Erstellung von Boxen (mit Inhalt) zu den markierten Zeitpunkten auf der Timeline
+        child: SizedBox.expand(
+          /// Die erstellten Boxen werden als klickbare Cards definiert
+          child: Card(
+            color: Colors.green,
+            /// Per Klick auf die jeweilige Card öffnet sich die "RidePage", die genauere Informationen zur jeweiligen Fahrt liefert
+            child: InkWell(
+              onTap: (){
+                  /// Rufe die "RidePage"-Seite auf
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => const RidePage(),
+                  ),
+                  );
+                },
+              child: Text('Ride $index'),
+              ),
+            ),
+          ),
+        ),
+      itemCount: 10,
+      ),
+    );
+  }
+
+}
+
+
+/*class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    String date = now.hour.toString() + ":" + now.minute.toString() + ":" + now.second.toString();
+    return SizedBox(
+      height: 240,
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              leading: Icon(Icons.album),
+              title: Text(""),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('BUY TICKETS'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+                TextButton(
+                  child: const Text('LISTEN'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
+
