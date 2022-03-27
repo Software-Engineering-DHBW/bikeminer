36be3c4ad7477704d1d4dcfe6216241be8cfd2b5
jeanz6 <jenju.woelpert@web.de>
2022-03-27 17:22:20 +0200

    fixed CreateHistory

--- api_bikeminer/sql_app/models.py
+++ api_bikeminer/sql_app/models.py
@@ -18,7 +18,7 @@ class Users(Base):
 class History(Base):
     __tablename__ = "History"
     historyID = Column(Integer, primary_key=True, index=True)
-    userID = Column(String, ForeignKey("Users.UserID"), index=True)
+    userID = Column(String, ForeignKey("Users.userID"), index=True)
     recievedCoins = Column(String)
     distanceTraveled = Column(Float)
     dateTime = Column(DateTime)
