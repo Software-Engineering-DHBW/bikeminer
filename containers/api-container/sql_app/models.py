af467f4311f17b7e6527da1fdb7deebdbfd671b0
jeanz6 <jenju.woelpert@web.de>
2022-03-29 14:01:51 +0200

    fastapi containerized

---
+++ containers/api-container/sql_app/models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from .database import Base
+
+# ORM Mapper for Database: Every Class represents one Database object
+# these classes are the sqlalchemy models
+
+class Users(Base):
+    __tablename__ = "Users"
+    userID = Column(Integer, primary_key=True)
+    userName = Column(String,unique=True)
+    password = Column(String)
+    email = Column(String, unique=True)
+    coins = Column(Float)
+
+    #history = relationship("History", back_populates=Users)
+
+class History(Base):
+    __tablename__ = "History"
+    historyID = Column(Integer, primary_key=True, index=True)
+    userID = Column(String, ForeignKey("Users.userID"), index=True)
+    receivedCoins = Column(String)
+    distanceTraveled = Column(Float)
+    dateTime = Column(DateTime)
+
+    def __str__(self) -> str:
+        return f"History: \nID: {self.historyID}\nUserID: {self.userID}\nCoins: {self.receivedCoins}\nDistance: {self.distanceTraveled}\nDatetime: {self.dateTime}"
+    # def __str__(self) -> str:
+    #     return f"History: \nID: {self.historyID}\nUserID: {self.userID}\nCoins: {self.receivedCoins}\nDistance: {self.distanceTraveled}\nDatetime: {self.dateTime}"
+
+    #users = relationship("Users", back_populates=History)
+
+class Coordinates(Base):
+    __tablename__ = "Coordinates"
+    coordID = Column(Integer, primary_key=True, index=True)
+    tourID = Column(Integer)
+    tourNumber = Column(Integer)
+    userID = Column(Integer, ForeignKey("Users.userID"), index=True)
+    longitude = Column(Float)
+    latitude = Column(Float)
+    datetime = Column(DateTime)
---
+++ containers/api-container/sql_app/schemas.py
@@ -0,0 +1,81 @@
+from typing import List, Optional
+from datetime import date, datetime
+from pydantic import BaseModel
+from fastapi.responses import FileResponse
+
+
+# BaseModels for FastAPI: Used as form_data and response objects --> Transforms db models to json objects
+
+class UserBase(BaseModel):
+    userName: str
+    email: str
+
+class UserBalance(BaseModel):
+    coins: float
+
+
+class UserCreate(UserBase):
+    userName: str
+    password: str
+
+
+
+class User(UserBase):
+    userID: int
+    userName: str
+    email: str
+    coins: float
+
+    class Config:
+        orm_mode = True
+
+class HistoryBase(BaseModel):
+    dateTime: datetime
+    distanceTraveled: float
+
+class HistoryCreate(HistoryBase):
+    receivedCoins: float
+
+    class Config:
+        orm_mode = True
+
+class History(HistoryBase):
+    historyID: int
+    userID: int
+
+    class Config:
+        orm_mode = True
+
+
+class CoordinatesCreate(BaseModel):
+    tourID: int
+    tourNumber: int
+    #userID: int
+    longitude: float
+    latitude: float
+    datetime: datetime
+class Coordinates(CoordinatesCreate):
+    coordID: int
+
+
+## for authentication
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+    project: Optional[str] = None
+
+
+
+
+
+
+
+
+
+
+
+
